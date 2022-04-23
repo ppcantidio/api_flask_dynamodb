e11f177283a04293a35e036efd2f92f48a96ad09
pedro <ppcantidio@gmail.com>
2022-04-23 18:05:08 -0300

    removing print

--- webapi/extensions/configuration.py
+++ webapi/extensions/configuration.py
@@ -6,7 +6,6 @@ def load_extensions(app):
     for extension in app.config.EXTENSIONS:
         # Split data in form `extension.path:factory_function`
         module_name, factory = extension.split(":")
-        print(module_name, factory)
         # Dynamically import extension module.
         ext = import_module(module_name)
         # Invoke factory passing app.
